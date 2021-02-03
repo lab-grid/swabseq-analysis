0eb8749e10fc49dd24c461f61c3ff89463200583
Robert Chu <allegrormc@gmail.com>
2021-02-03 15:54:38 -0800

    Include access/error logs in stderr.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ if [[ ! -z "${BASESPACE_CFG}" ]]; then
 fi
 
 if [[ -z "${@}" ]]; then
-    python3 -m gunicorn.app.wsgiapp --timeout 240 --bind 0.0.0.0:${PORT} --workers 4 main:app
+    python3 -m gunicorn.app.wsgiapp --timeout 240 --bind 0.0.0.0:${PORT} --access-logfile - --error-logfile - --workers 4 main:app
 else
     ${@}
 fi
